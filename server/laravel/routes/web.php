cbad035ac12d35334877bb8a0d6168fc8ca24a2f
Takashi Saito <takashi11171117@gmail.com>
2021-02-21 17:22:43 +0900

    add models and controllers

--- server/laravel/routes/web.php
+++ server/laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\EpisodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +22,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/articles/{article:slug}', ArticleController::class);
+Route::get('/episodes/{episode:slug}', EpisodeController::class);
 
 require __DIR__.'/auth.php';
